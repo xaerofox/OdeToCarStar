61ee34b833c69f711d36bf06f7d4a509d51b9ea1
joeyt <xaerofox@gmail.com>
2023-11-12 14:08:24 -0500

    Updates retrofit builder to use http client with SimpleAuthInterceptor

--- app/src/main/java/com/jtor/odetocarstar/core/di/AppModule.kt
+++ app/src/main/java/com/jtor/odetocarstar/core/di/AppModule.kt
@@ -2,11 +2,17 @@ package com.jtor.odetocarstar.core.di
 
 import com.jtor.odetocarstar.BuildConfig
 import com.jtor.odetocarstar.data.remote.CarApi
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
 import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -16,8 +22,18 @@ object AppModule {
     @Provides
     @Singleton
     fun providesCarApi(): CarApi {
+        val moshi = Moshi.Builder()
+            .addLast(KotlinJsonAdapterFactory())
+            .build()
+
+        val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor(SimpleAuthInterceptor())
+            .build()
+
         return Retrofit.Builder()
             .baseUrl(BuildConfig.BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .client(okHttpClient)
             .build()
             .create(CarApi::class.java)
     }
@@ -27,4 +43,17 @@ object AppModule {
 //    fun provideCarRepository(api: CarApi): CarRepository {
 //        return CarRepositoryImpl(api)
 //    }
+}
+
+class SimpleAuthInterceptor: Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest = chain.request()
+
+        val updatedRequest = originalRequest.newBuilder()
+            .header("X-RapidAPI-Key", BuildConfig.RAPID_API_KEY)
+            .header("X-RapidAPI-Host", BuildConfig.RAPID_HOST)
+            .build()
+
+        return chain.proceed(updatedRequest)
+    }
 }
