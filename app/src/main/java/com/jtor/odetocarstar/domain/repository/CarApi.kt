b3831f51aef82dd985a1931b010c137b8714b1ee
Jose Torres <xaerofox@gmail.com>
2023-11-22 14:13:17 -0500

    Updates getMakes with some query parameters

--- app/src/main/java/com/jtor/odetocarstar/domain/repository/CarApi.kt
+++ app/src/main/java/com/jtor/odetocarstar/domain/repository/CarApi.kt
@@ -3,9 +3,13 @@ package com.jtor.odetocarstar.domain.repository
 import com.jtor.odetocarstar.data.remote.dto.CollectionDto
 import com.jtor.odetocarstar.domain.model.CarMake
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface CarApi {
 
     @GET("/api/makes")
-    suspend fun getMakes() : CollectionDto<CarMake>
+    suspend fun getMakes(
+        @Query("year") year: Int = 2015,
+        @Query("sort") sort: String = "name"
+    ) : CollectionDto<CarMake>
 }
