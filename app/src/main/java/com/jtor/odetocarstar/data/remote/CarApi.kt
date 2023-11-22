c8aa636cf0633564c4367e0825d961678430a6d1
Jose Torres <xaerofox@gmail.com>
2023-11-22 13:52:07 -0500

    Updates CarApi to return response data

--- app/src/main/java/com/jtor/odetocarstar/data/remote/CarApi.kt
+++ app/src/main/java/com/jtor/odetocarstar/data/remote/CarApi.kt
@@ -1,9 +1,11 @@
 package com.jtor.odetocarstar.data.remote
 
+import com.jtor.odetocarstar.data.models.CarApiCollection
+import com.jtor.odetocarstar.data.models.CarMake
 import retrofit2.http.GET
 
 interface CarApi {
 
     @GET("/api/makes")
-    suspend fun getMakes()
+    suspend fun getMakes() : CarApiCollection<CarMake>
 }
