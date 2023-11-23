452557e1f181cff18617c85bdb721610ae93866d
Jose Torres <xaerofox@gmail.com>
2023-11-22 20:38:25 -0500

    Updates CarRepository method

--- app/src/main/java/com/jtor/odetocarstar/data/repository/CarRepositoryImpl.kt
+++ app/src/main/java/com/jtor/odetocarstar/data/repository/CarRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.jtor.odetocarstar.data.repository
 
+import com.jtor.odetocarstar.domain.model.CarMake
 import com.jtor.odetocarstar.domain.repository.CarApi
 import com.jtor.odetocarstar.domain.repository.CarRepository
 import javax.inject.Inject
@@ -7,7 +8,13 @@ import javax.inject.Inject
 class CarRepositoryImpl @Inject constructor(
     private val api: CarApi
 ) : CarRepository {
-    override suspend fun getMakes() {
-
+    override suspend fun getMakes(
+        year: Int?,
+        sort: String?
+    ) : List<CarMake> {
+        return api.getMakes(
+            year = year ?: 2015,
+            sort = sort
+        ).data
     }
 }
--- app/src/main/java/com/jtor/odetocarstar/domain/repository/CarRepository.kt
+++ app/src/main/java/com/jtor/odetocarstar/domain/repository/CarRepository.kt
@@ -1,5 +1,7 @@
 package com.jtor.odetocarstar.domain.repository
 
+import com.jtor.odetocarstar.domain.model.CarMake
+
 interface CarRepository {
-    suspend fun getMakes()
+    suspend fun getMakes(year: Int?, sort: String?) : List<CarMake>
 }
