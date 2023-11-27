804423468ba9cc20ae4fbeedbaa6d3c85ca77544
Jose Torres <xaerofox@gmail.com>
2023-11-27 15:49:47 -0500

    Adds scaffold to create top app bar

--- app/src/main/java/com/jtor/odetocarstar/presentation/makes/CarMakeScreen.kt
+++ app/src/main/java/com/jtor/odetocarstar/presentation/makes/CarMakeScreen.kt
@@ -7,7 +7,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -19,36 +24,56 @@ import androidx.navigation.NavController
 import com.jtor.odetocarstar.presentation.Screen
 import com.jtor.odetocarstar.presentation.makes.components.MakeListItem
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CarMakeScreen(
     navController: NavController,
     viewModel: CarMakeViewModel = hiltViewModel()
 ) {
-    val state = viewModel.state.value
-    Box(modifier = Modifier.fillMaxSize()) {
-        LazyColumn(modifier = Modifier.fillMaxSize()) {
-            items(state.makes) { make ->
-                MakeListItem(make = make, onItemClick = {
-                    navController.navigate(Screen.CarYearScreen.withArgs(make.name))
-                })
-
-            }
-        }
-
-        if (state.error.isNotBlank()) {
-            Text(
-                text = state.error,
-                color = Color.Red,
-                textAlign = TextAlign.Center,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 20.dp)
-                    .align(Alignment.Center)
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Ode To Car*")
+                },
+                colors = topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary
+                )
             )
         }
+    ) {
+        val state = viewModel.state.value
+        Box(
+            modifier = Modifier
+                .padding(it)
+                .fillMaxSize()
+        ) {
+            LazyColumn(modifier = Modifier.fillMaxSize()) {
+                items(state.makes) { make ->
+                    MakeListItem(make = make, onItemClick = {
+                        navController.navigate(Screen.CarYearScreen.withArgs(make.name))
+                    })
 
-        if(state.isLoading) {
-            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+                }
+            }
+
+            if (state.error.isNotBlank()) {
+                Text(
+                    text = state.error,
+                    color = Color.Red,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 20.dp)
+                        .align(Alignment.Center)
+                )
+            }
+
+            if (state.isLoading) {
+                CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+            }
         }
     }
+
 }
