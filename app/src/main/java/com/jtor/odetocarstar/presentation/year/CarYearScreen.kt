bd316ef7c0779e8944bf2dfaa8996b3a954bd2c4
Jose Torres <xaerofox@gmail.com>
2023-12-09 18:15:21 -0500

    Updates UI to add in a top bar and back navigation

--- app/src/main/java/com/jtor/odetocarstar/presentation/year/CarYearScreen.kt
+++ app/src/main/java/com/jtor/odetocarstar/presentation/year/CarYearScreen.kt
@@ -5,41 +5,70 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CarYearScreen(
     navController: NavController? = null,
     make: String
 ) {
-    Column(
-        modifier = Modifier
-            .padding(20.dp)
-            .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Select a year for $make")
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary
+                ),
+                navigationIcon = {
+                    IconButton(onClick = { navController?.popBackStack() }) {
+                        Icon(Icons.Default.ArrowBack, contentDescription = null)
+                    }
+                }
+            )
+        }
     ) {
-        Text(
-            text = "Select a year for $make",
-            fontSize = 20.sp
-        )
-        val years = listOf("2015", "2016", "2017", "2018", "2019", "2020")
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(2)
+        Column(
+            modifier = Modifier
+                .padding(it)
+                .padding(10.dp)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            items(years.size) {
-                OutlinedButton(
-                    modifier = Modifier.padding(4.dp),
-                    onClick = { /*TODO*/ }
-                ) {
-                    Text(text = years[it])
+            //        Text(
+            //            text = "Select a year for $make",
+            //            fontSize = 20.sp
+            //        )
+            val years = listOf("2015", "2016", "2017", "2018", "2019", "2020")
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2)
+            ) {
+                items(years.size) {
+                    OutlinedButton(
+                        modifier = Modifier.padding(4.dp),
+                        onClick = { /*TODO*/ }
+                    ) {
+                        Text(text = years[it])
+                    }
                 }
             }
         }
