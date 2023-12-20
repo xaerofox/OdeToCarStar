6e9752ab93cc529e2bc62aaa54aff77d1dea866a
Jose Torres <xaerofox@gmail.com>
2023-12-20 13:17:00 -0500

    Adds the Coil library to project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,6 +83,9 @@ dependencies {
     implementation(libs.converter.moshi)
     implementation(libs.moshi.kotlin)
 
+    //Coil
+    implementation(libs.coil.compose)
+
     testImplementation(libs.junit)
 
     androidTestImplementation(libs.androidx.junit)
