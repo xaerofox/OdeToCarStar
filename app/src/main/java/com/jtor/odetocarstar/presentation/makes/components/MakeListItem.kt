b3434a06821982b7cefe6b56b42d755f4892fe01
Jose Torres <xaerofox@gmail.com>
2023-12-20 13:51:20 -0500

    Adds async images of automaker logos

--- app/src/main/java/com/jtor/odetocarstar/presentation/makes/components/MakeListItem.kt
+++ app/src/main/java/com/jtor/odetocarstar/presentation/makes/components/MakeListItem.kt
@@ -1,19 +1,28 @@
 package com.jtor.odetocarstar.presentation.makes.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.jtor.odetocarstar.domain.model.CarMake
+import com.jtor.odetocarstar.presentation.util.Constants
 import com.jtor.odetocarstar.presentation.util.theme.OdeToCarStarTheme
 
 @Composable
@@ -21,24 +30,38 @@ fun MakeListItem(
     make: CarMake,
     onItemClick: (CarMake) -> Unit
 ) {
-    Row(
+    Box(
         modifier = Modifier
             .fillMaxWidth()
             .clickable { onItemClick(make) }
-            .padding(20.dp),
-        horizontalArrangement = Arrangement.Start
+            .padding(20.dp)
     ) {
-        Text(
-            text = "${make.id}.",
-            style = MaterialTheme.typography.headlineMedium
+        AsyncImage(
+            model = findMakeLogo(make.name.lowercase()),
+            contentDescription = null,
+            modifier = Modifier
+                .width(100.dp)
+                .height(100.dp)
+                .clip(RoundedCornerShape(10.dp))
+                .aspectRatio(100f/100f, true)
+                .background(Color.White)
         )
-        Spacer(modifier = Modifier.width(8.dp))
-        Text(
-            text = make.name,
-            style = MaterialTheme.typography.headlineMedium
-        )
+//
+//        Text(
+//            text = "${make.id}.",
+//            style = MaterialTheme.typography.headlineMedium
+//        )
+//        Spacer(modifier = Modifier.width(8.dp))
+//        Text(
+//            text = make.name,
+//            style = MaterialTheme.typography.headlineMedium
+//        )
 
     }
+}
+
+fun findMakeLogo(make: String): String {
+    return Constants.IMAGE_BASE_URL + Constants.IMAGE_PATH_THUMB + make.replace(" ", "-") + ".png?raw=true"
 }
 
 @Preview(showBackground = true)
---
+++ app/src/main/java/com/jtor/odetocarstar/presentation/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.jtor.odetocarstar.presentation.util
+
+object Constants {
+    const val IMAGE_BASE_URL = "https://github.com/filippofilip95/car-logos-dataset/blob/master/logos/"
+    const val IMAGE_PATH_THUMB = "thumb/"
+    const val IMAGE_PATH_ORIGINAL = "original/"
+    const val IMAGE_PATH_OPTIMIZED = "optimized/"
+}
