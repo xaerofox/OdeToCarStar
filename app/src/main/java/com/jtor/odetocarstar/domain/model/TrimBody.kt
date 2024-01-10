f9a12797e4e6178a46ed9f65b7bee8d4cc3ca671
Jose Torres <xaerofox@gmail.com>
2024-01-10 14:53:40 -0500

    Makes detail data optional

--- app/src/main/java/com/jtor/odetocarstar/domain/model/TrimBody.kt
+++ app/src/main/java/com/jtor/odetocarstar/domain/model/TrimBody.kt
@@ -4,32 +4,32 @@ import com.squareup.moshi.Json
 
 data class TrimBody(
     @Json(name = "cargo_capacity")
-    val cargoCapacity: String,
+    val cargoCapacity: String?,
     @Json(name = "curb_weight")
-    val curbWeight: Int,
-    val doors: Int,
+    val curbWeight: Int?,
+    val doors: Int?,
     @Json(name = "front_track")
-    val frontTrack: String,
+    val frontTrack: String?,
     @Json(name = "gross_weight")
-    val grossWeight: String,
+    val grossWeight: String?,
     @Json(name = "ground_clearance")
-    val groundClearance: String,
-    val height: String,
-    val id: Int,
-    val length: String,
+    val groundClearance: String?,
+    val height: String?,
+    val id: Int?,
+    val length: String?,
     @Json(name = "make_model_trim_id")
-    val trimId: Int,
+    val trimId: Int?,
     @Json(name = "max_cargo_capacity")
-    val maxCargoCapacity: String,
+    val maxCargoCapacity: String?,
     @Json(name = "max_payload")
-    val maxPayload: String,
+    val maxPayload: String?,
     @Json(name = "max_towing_capacity")
-    val maxTowingCapacity: String,
+    val maxTowingCapacity: String?,
     @Json(name = "rear_track")
-    val rearTrack: String,
-    val seats: Int,
-    val type: String,
+    val rearTrack: String?,
+    val seats: Int?,
+    val type: String?,
     @Json(name = "wheel_base")
-    val wheelBase: String,
-    val width: String
+    val wheelBase: String?,
+    val width: String?
 )
