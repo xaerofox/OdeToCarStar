ffe57e521a6b7c11542784caf00ef82fb191a2bf
Jose Torres <xaerofox@gmail.com>
2024-01-10 15:03:11 -0500

    Tweaks TrimElementItem.kt

--- app/src/main/java/com/jtor/odetocarstar/presentation/trims/components/TrimElementItem.kt
+++ app/src/main/java/com/jtor/odetocarstar/presentation/trims/components/TrimElementItem.kt
@@ -2,7 +2,10 @@ package com.jtor.odetocarstar.presentation.trims.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -10,6 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -18,8 +22,12 @@ fun TrimElementItem(name: String, value: String) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
+            .padding(
+                horizontal = 12.dp,
+                vertical = 4.dp
+            )
             .drawBehind {
-                        val borderSize = 2.dp.toPx()
+                val borderSize = 2.dp.toPx()
                 drawLine(
                     color = Color.LightGray,
                     start = Offset(0f, size.height),
@@ -27,16 +35,19 @@ fun TrimElementItem(name: String, value: String) {
                     strokeWidth = borderSize
                 )
             },
-        horizontalArrangement = Arrangement.SpaceAround
+        horizontalArrangement = Arrangement.SpaceBetween
     ) {
         Text(
-            text = "$name",
+            text = name,
             style = MaterialTheme.typography.bodyMedium
         )
 
+        Spacer(modifier = Modifier.width(8.dp))
+
         Text(
-            text = "$value",
-            style = MaterialTheme.typography.bodyMedium
+            text = value,
+            style = MaterialTheme.typography.bodyMedium,
+            textAlign = TextAlign.Right
         )
     }
 }
