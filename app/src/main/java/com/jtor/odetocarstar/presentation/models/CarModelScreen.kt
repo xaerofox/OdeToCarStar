0b89594a03052dbc71bc8c4e275ed4414aa42359
Jose Torres <xaerofox@gmail.com>
2025-06-08 16:08:07 -0400

    Updates deprecated icon

--- app/src/main/java/com/jtor/odetocarstar/presentation/models/CarModelScreen.kt
+++ app/src/main/java/com/jtor/odetocarstar/presentation/models/CarModelScreen.kt
@@ -7,7 +7,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -60,7 +60,7 @@ fun CarModelScreen(
                 ),
                 navigationIcon = {
                     IconButton(onClick = { navController?.popBackStack() }) {
-                        Icon(Icons.Default.ArrowBack, contentDescription = null)
+                        Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = null)
                     }
                 }
             )
