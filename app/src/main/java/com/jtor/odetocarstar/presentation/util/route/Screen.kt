dbbe6cf6cb1a73c50e03e26b15799de75239d7b8
Jose Torres <xaerofox@gmail.com>
2025-07-27 20:08:34 -0400

    Tweaks to Screen.kt

--- app/src/main/java/com/jtor/odetocarstar/presentation/util/route/Screen.kt
+++ app/src/main/java/com/jtor/odetocarstar/presentation/util/route/Screen.kt
@@ -1,10 +1,10 @@
 package com.jtor.odetocarstar.presentation.util.route
 
 sealed class Screen(val route: String) {
-    object CarMakeScreen: Screen("car_makes")
-    object CarYearScreen: Screen("car_year")
-    object CarModelScreen: Screen("car_models")
-    object CarTrimScreen: Screen("car_trim")
+    data object CarMakeScreen: Screen("car_makes")
+    data object CarYearScreen: Screen("car_year")
+    data object CarModelScreen: Screen("car_models")
+    data object CarTrimScreen: Screen("car_trim")
 
     fun withArgs(vararg args: String): String {
         return buildString {
